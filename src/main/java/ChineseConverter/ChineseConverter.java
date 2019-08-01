1bccc2c6a0f9dca8ce47fb6cd0de2d01f7c92968
Chung-Yi Chao <tony9927226@gmail.com>
2019-08-01 15:04:59 +0800

    add Transfer type update class name

--- src/main/java/ChineseConverter/ChineseConverter.java
+++ src/main/java/ChineseConverter/ChineseConverter.java
@@ -11,8 +11,13 @@ import com.github.houbb.opencc4j.util.ZhConverterUtil;
  * Traditional chinese transform to simple chinese
  * by opencc4j
  */
-public class TtoSChineseConverter {
-    public static void convertFile(String absFilePath) {
+public class ChineseConverter {
+    public enum TransferType {
+        T_TO_S,
+        S_TO_T
+    }
+
+    public static void convertFile(String absFilePath, TransferType type) {
         StringBuilder oldContentBuilder = new StringBuilder();
         BufferedReader bufferedReader = null;
         FileWriter fileWriter = null;
@@ -24,7 +29,12 @@ public class TtoSChineseConverter {
                 line = bufferedReader.readLine();
             } while (line != null);
             if (!("null" + System.lineSeparator()).equals(oldContentBuilder.toString())) {
-                String newContent = ZhConverterUtil.convertToSimple(oldContentBuilder.toString());
+                String newContent;
+                if (type == TransferType.T_TO_S) {
+                    newContent = ZhConverterUtil.convertToSimple(oldContentBuilder.toString());
+                } else {
+                    newContent = ZhConverterUtil.convertToTraditional(oldContentBuilder.toString());
+                }
 
                 fileWriter = new FileWriter(absFilePath);
 
@@ -45,7 +55,7 @@ public class TtoSChineseConverter {
         }
     }
 
-    public static void convertFolder(String folderPath) {
-        FolderFileGetter.getFilePathList(folderPath).stream().forEach(filePath -> convertFile(filePath));
+    public static void convertFolder(String folderPath, TransferType type) {
+        FolderFileGetter.getFilePathList(folderPath).stream().forEach(filePath -> convertFile(filePath, type));
     }
 }
